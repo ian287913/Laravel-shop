fbcf378a4057fd2b8efa300e9c18a0a89bc12b96
Will <will889889@gmail.com>
2018-12-06 21:19:42 +0800

    修改seeder以及brand結構

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -19,35 +19,40 @@ class ProductsTableSeeder extends Seeder
         OperationSystem::truncate();
 
         $faker = Faker\Factory::create('zh_TW');
-        $total = 5;
-        $brand = ['MSI', 'ASUS', 'ACER'];
-        $OS = ['WINDOWS','MACOS','LINUX'];
+        $total = 30;
+        $brand = ['Apple','MSI', 'ASUS', 'Acer','Lenovo','Hp'];
+        $OS = ['Windows','MacOS','Linux'];
 
-        foreach (range(0,2) as $i) {
-            $category = Category::create([
-                'name'=>$brand[$i],
+        $os = [OperationSystem::create([
+            'name'=>$OS[0],
+        ]), OperationSystem::create([
+            'name'=>$OS[1],
+        ]), OperationSystem::create([
+            'name'=>$OS[2],
+        ])];
+        foreach (range(0,5) as $i) {
+            Category::create([
+                'name' => $brand[$i],
             ]);
-            $os = OperationSystem::create([
-                'name'=>$OS[$i],
-            ]);
-            foreach (range(1, $total) as $id){
-                Product::create([
-                    'name'=>$faker->realText(rand(10,15)),
-                    'price'=>rand(100,1000),
-                    'size'=>$faker->realText(rand(10,15)),
-                    'cpu'=>$faker->realText(rand(10,15)),
-                    'gpu'=>$faker->realText(rand(10,15)),
-                    'ram'=>$faker->realText(rand(10,15)),
-                    'storage'=>$faker->realText(rand(10,15)),
-                    'description'=>$faker->realText(rand(10,15)),
-                    'category_id'=>$category->id,
-                    'os_id'=>$os->id,
-                    'img'=>'storage/images/nb.jpg',
-                    'created_at' => now()->subDays($total - $id)->addHours(rand(1, 5))->addMinutes(rand(1, 5)),
-                    'updated_at' => now()->subDays($total - $id)->addHours(rand(6, 10))->addMinutes(rand(10, 30)),
-                ]);
-            }
         }
+        foreach (range(1, $total) as $id){
+            Product::create([
+                'name'=>$faker->realText(rand(10,15)),
+                'price'=>rand(100,1000),
+                'size'=>$faker->realText(rand(10,15)),
+                'cpu'=>$faker->realText(rand(10,15)),
+                'gpu'=>$faker->realText(rand(10,15)),
+                'ram'=>$faker->realText(rand(10,15)),
+                'storage'=>$faker->realText(rand(10,15)),
+                'description'=>$faker->realText(rand(10,15)),
+                'category_id'=>rand(0,5),
+                'os_id'=>$os[rand(0,2)]->id,
+                'img'=>'storage/images/nb.jpg',
+                'created_at' => now()->subDays($total - $id)->addHours(rand(1, 5))->addMinutes(rand(1, 5)),
+                'updated_at' => now()->subDays($total - $id)->addHours(rand(6, 10))->addMinutes(rand(10, 30)),
+            ]);
+        }
+
 
     }
 }
